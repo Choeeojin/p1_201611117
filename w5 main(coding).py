b902552218b68bb88c98ae0d4211e41ab41e4bb4
Choeeojin <djwls222@naver.com>
2016-03-30 15:26:00 +0900

    w5 score into grade

---
+++ w5 main(coding).py
@@ -0,0 +1,24 @@
+﻿raw_input("enter score (0~100): ")
+Score=raw_input("enter score (0~100): ")
+Score=float(Score)
+
+
+def computeGrade(Score):
+    if (90<=Score<=100):
+        grade='A'
+
+    elif (80<=Score<90):
+        grade='B'
+
+    elif (70<=Score<80):
+        grade='C'
+
+    elif (60<=Score<70):
+        grade='D' 
+
+    elif (Score<60):
+        grade='F'
+        
+    return grade
+
+computeGrade(90)
