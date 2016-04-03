e502cc3f88b3987216681b436de765bfccb81597
Choeeojin <djwls222@naver.com>
2016-04-03 20:50:04 +0900

    rick scissor paper

---
+++ w5 main (rcp).py
@@ -0,0 +1,20 @@
+﻿teamA=raw_input("What is teamA's choice?: ")
+teamB=raw_input("What is teamB's choice?: ")
+
+if (teamA==teamB):
+        print "Draw"
+    elif (teamA=="r"):
+        if (teamB=="s"):
+            print "teamA is Win"
+        elif (teamB=="p"):
+                print "teamB is Win"
+    elif (teamA=="s"):
+        if (teamB=="r"):
+            print "teamB is Win"
+        elif (teamB=="p");
+            print "teamA is Win"
+    elif (teamA=="p"):
+        if (teamB=="r"):
+            print "teamA is Win"
+        elif (temaB=="s")
+            print "teamB is Win"
