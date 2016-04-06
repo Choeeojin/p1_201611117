51d90ff40fd7cba37d29a74be90ac64d9472397a
Choeeojin <djwls222@naver.com>
2016-04-06 15:31:07 +0900

    w6 3 or 5

---
+++ w6 Main.py
@@ -0,0 +1,30 @@
+﻿"""
+@author cej
+@since 160406
+"""
+
+
+begin=0
+end=1000
+
+def sumofMultiplesof3_5(begin,end):
+    sum=0
+
+    for i in range(begin,end):
+        if i%3==0 or i%5==0:
+            sum=sum+i
+    print sum
+    return sum
+
+
+sumofMultiplesof3_5(1,1000)
+
+def lab6():
+	begin=0
+	end=1000
+	sumofMultiplesof3_5(1,1000)
+
+def main():
+	lab6()
+if __name__=="__main__"
+	main()
