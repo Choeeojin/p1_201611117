f3da4c64629b43d792168377fa9d4ef84cb4d899
Choeeojin <djwls222@naver.com>
2016-04-06 15:31:21 +0900

    w6(11) leapyear

---
+++ w6(11) Main.py
@@ -0,0 +1,30 @@
+﻿"""
+@author cej
+@since 160406
+"""
+
+
+year=raw_input("Year? ")
+
+
+def leapYear(year):
+    if year%4==0 and not year%100==0:
+        print year
+    elif year%4==0 and year%400==0:
+        print year
+    else:
+        print "error"
+
+
+
+
+
+
+def lab6():
+	year=raw_input("Year? ")
+	leapYear(year)
+
+def main():
+	lab6()
+if __name__=="__main__"
+	main()
