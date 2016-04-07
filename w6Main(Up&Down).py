9e1468133b38f08c7e3d40475a4bb03c59da80ac
Choeeojin <djwls222@naver.com>
2016-04-08 01:36:26 +0900

    UpandDownGame

---
+++ w6Main(Up&Down).py
@@ -0,0 +1,31 @@
+﻿"""
+@author cej
+@since 160408
+"""
+
+
+
+host=raw_input("Enter the number ")
+
+
+def UpandDownGame():
+    player=raw_input("Enter the number you think ")
+    if host==player:
+        print "Pass"
+    elif host>player:
+        print "Up"
+        UpandDownGame()
+    elif host<player:
+        print "Down"
+ 	UpandDownGame()
+
+
+
+
+def lab6():
+	UpandDownGame()
+
+def main():
+	lab6()
+if __name__=="__main__"
+	main()
