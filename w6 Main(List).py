9f98429e320856b41d44725d42c9778e6dfa5b53
Choeeojin <djwls222@naver.com>
2016-04-11 20:08:06 +0900

    List function

---
+++ w6 Main(List).py
@@ -0,0 +1,30 @@
+﻿"""
+@author cej
+@since 160411
+"""
+
+d=list()
+
+for i in range(0,1001):
+    if i%4==0 and i%5>0:
+        d.append(i)
+
+
+def sumList(d):
+    sum=0
+    for j in range(0,len(d)):
+        sum=sum+d[j]
+    return sum
+        
+
+print sumList(d)
+
+def lab6():
+	mysum=sumList(d)
+	print mysum
+
+def main():
+	lab6()
+
+if __name__=="__main__"
+	main()
