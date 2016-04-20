e34cea1c3c6de035183218addcf2b74a38952de0
Choeeojin <djwls222@naver.com>
2016-04-20 12:24:08 +0900

    dict함수로 도형그리기

---
+++ w7main(dict).py
@@ -0,0 +1,26 @@
+﻿"""
+@author cej
+@since 160420
+"""
+
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def drawSquareFrom(size):
+    tracks=dict({1:(size,0),2:(size,-size),3:(0,-size),4:(0,0)})
+    for i in range(1,5):
+        t1.goto(tracks[i])
+
+drawSquareFrom(100)
+
+wn.exitonclick()
+
+def lab7():
+	drawSquareFrom(100)
+
+def main():
+	lab6()
+
+if __name__=="__main__"
+	main()
