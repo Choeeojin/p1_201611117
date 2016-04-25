04bc14c829d31fcd1d7a2318c040dda62b7ff7cd
Choeeojin <djwls222@naver.com>
2016-04-25 11:55:46 +0900

    0.5ver

---
+++ PythonGame 0.5ver.py
@@ -0,0 +1,29 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+t1.home()
+t1.clear()
+t1.penup()
+
+
+
+def k1():
+	move.forward(45)
+
+def k2():
+	move.left(45)
+
+def k3():
+	move.right(45)
+
+def k4():
+	move.back(45)
+
+wn.onkey(k1, "Up")
+wn.onkey(k2, "Left")
+wn.onkey(k3, "Right")
+wn.onkey(k4, "Down")
+
+
+
+wn.exitonclick()
