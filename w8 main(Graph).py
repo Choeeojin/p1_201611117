fe39400d7949d2972b8fecc107ad6c4998748b6c
Choeeojin <djwls222@naver.com>
2016-05-02 13:14:59 +0900

    문자 쪼개서 갯수를 세고 이를 이용하여 그래프 그리기

---
+++ w8 main(Graph).py
@@ -0,0 +1,21 @@
+﻿def charCount(word):
+    d=dict()
+
+    for c in word:
+        if c not in d:
+            d[c]=1
+            print d
+        else:
+            d[c]=d[c]+1
+    print d
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    plt.bar(range(len(d)),d.values(),align='center')
+    plt.xticks(range(len(d)),list(d.keys()))
+    plt.show()
+
+def lab8():
+    charCount('sangmyung university')
+def main():
+    lab8()
