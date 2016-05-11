c3f57e436f23d2d9395d2fd2faec0997ffce301e
Choeeojin <djwls222@naver.com>
2016-05-11 11:36:01 +0900

    w10 dataconstructure

---
+++ w10 main(Milk)..py
@@ -0,0 +1,29 @@
+﻿"""
+@author cej
+@since 160509
+"""
+coffee=list()
+coffee=[["Coffee","Water","Milk","Icecream"]
+  ,["Espresso","No","No","No"]
+  ,["Long Black","Yes","No","No"]
+  ,["Flat White","No","Yes","No"]
+  ,["Cappuccino","No","Yes-Fronthy","No"]
+  ,["Affogato","No","No","Yes"]]
+data=coffee[1:len(coffee)]
+print data
+for i in data:
+    if i[2].upper()=="YES":
+        print i[0]
+
+ms=0
+nms=0
+for i in data:
+    if i[0]=="Flat White":
+        i[2]=1
+        ms=i[2]+ms
+        print ms
+    else:
+        i[2]=1
+        nms=i[2]+nms
+print ms/(nms+ms)
+
---
+++ w10 main(allData).py
@@ -0,0 +1,19 @@
+﻿"""
+@author cej
+@since 160509
+"""
+alldata=list()
+alldata=[["English",100,"Math",200],["English",200,"Math",200],["English",100,"Math",300]]
+es=0
+ms=0
+for i in alldata:
+    es=i[1]+es
+    print es
+
+for c in alldata:
+    ms=c[3]+ms
+    print ms
+ea=es/len(alldata)
+ma=ms/len(alldata)
+print ea
+print ma
