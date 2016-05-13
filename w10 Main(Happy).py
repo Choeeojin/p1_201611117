d4dd7bd2a098f913fcd0663b9b02c42cd1215f29
Choeeojin <djwls222@naver.com>
2016-05-13 16:43:14 +0900

    과제

---
+++ w10 Main(Happy).py
@@ -0,0 +1,56 @@
+﻿alldata = [('content', 13.1, 37.1, 8.7, 1.5),
+('method', 10.6, 34.6, 13.4, 1.9),
+('strelation', 27.1, 40.0, 2.9, 1.5),
+('prrelation', 16.2, 37.8, 6.8, 0.8),
+('equipment', 11.4, 29.8, 14.8, 4.9),
+('enviroment', 12.2, 26.5, 14.9, 4.4),
+('subject', 13.5, 29.7, 11.1, 2.4),
+('school', 13.7, 37.6, 4.1, 1.2)]
+
+def data():
+    best=list()
+    for i in range(0,len(alldata)):
+        best.append(alldata[i][1])
+    good=list()
+    for i in range(0,len(alldata)):
+        good.append(alldata[i][2])
+    bad=list()
+    for i in range(0,len(alldata)):
+        bad.append(alldata[i][3])
+    worst=list()
+    for i in range(0,len(alldata)):
+        worst.append(alldata[i][4])
+    sumb=0
+    for i in range(0,len(alldata)):
+        sumb = sumb + best[i]
+    sumg = 0
+    for i in range(0,len(alldata)):
+        sumg = sumg + good[i]
+    goodsum = 0
+    goodsum = sumb + sumg
+    print "Best and Good sum ", goodsum
+    average = 0
+    gaverage = goodsum / len(alldata)
+    print "Best & Good average ", gaverage
+
+    sumb=0
+    for i in range(0,len(alldata)):
+        sumb = sumb + bad[i]
+    sumw=0
+    for i in range(0,len(alldata)):
+        sumw = sumw + worst[i]
+    badsum = 0
+    badsum = sumb + sumw
+    print "Worst & Bad sum", badsum
+
+    baverage = 0
+    baverage = badsum / len(alldata)
+    print "Worst & Bad average", baverage
+    
+def lab10():
+    data()
+def main():
+    lab10()
+    raw_input()
+if __name__=="__main__":
+    main()
---
+++ w11 Main.py
@@ -0,0 +1,52 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def drawLine():
+    t1.penup()
+    t1.setpos(-100,0)
+    t1.pendown()
+    t1.forward(100)
+    t1.penup()
+    t1.home()
+
+drawLine()
+
+x=list()
+for i in range(-100,0):
+    x.append(i)
+if t1.pos(x[i],0):
+    print "t1 on Line"
+else:
+    print "Keep going"
+
+
+def keyup():
+    t1.forward(50)
+def keydown():
+    t1.back(50)
+def keyright():
+    t1.right(90)
+def keyleft():
+    t1.left(90)
+def keybye():
+    wn.bye()
+
+def mousegoto(x,y):
+    t1.setpos(x,y)
+
+def addkeys():
+    wn.onkey(keyup,"Up")
+    wn.onkey(keydown,"Down")
+    wn.onkey(keyright,"Right")
+    wn.onkey(keyleft,"Left")
+    wn.onkey(keybye,"q")
+
+def addMouse():
+    wn.onclick(mousegoto)
+
+addMouse()
+addkeys()
+wn.listen()
+turtle.mainloop()
+
