df5e58eb8d0f4db8996c02a622e70fb7d9562e95
Choeeojin <djwls222@naver.com>
2016-05-24 21:01:12 +0900

    'file

---
+++ w12 Main.py
@@ -0,0 +1,22 @@
+﻿import os
+mydir=os.getcwd()
+print mydir
+filename='python.txt'
+myfilename=os.path.join(mydir,filename)
+myfile='C:\Users\eojing\Desktop\\python.txt'
+myfile=open(myfilename,'r')
+contents=myfile.read()
+for i in range(0,len(contents)):
+    if contents[i].find('Python')>=0:
+        print contents[i]
+    else:
+        print "Not found"
+myfile.close()
+
+myfile=open(myfilename,'r')
+for line in myfile:
+    if line.upper()=='LINE':
+        print line
+    else:
+        print "Not found"
+myfile.close()
