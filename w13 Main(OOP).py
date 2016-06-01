eaf3d88f36dd262e68fbc60cfbedf7da6de1bb80
Choeeojin <djwls222@naver.com>
2016-06-01 16:12:41 +0900

    객체지향 짖어봐강아지야

---
+++ w13 Main(OOP).py
@@ -0,0 +1,33 @@
+﻿
+
+
+class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is ", self.name
+    def talk(self):
+        print "mung mung", self.name
+        
+class ShihTzu(Dog):
+    def talk(self):
+        print "si si", self.name
+
+class Maltese(Dog):
+    def talk(self):
+        print "mal mal", self.name
+
+def lab13():
+    mydog1=Dog('Dog')
+    mydog1.talk()
+
+    mydog2=ShihTzu('ShihTzu')
+    mydog2.talk()
+
+    mydog3=Maltese('Maltese')
+    mydog3.talk()
+
+def main():
+    lab13()
+if __name__=="__main__":
+    main()
